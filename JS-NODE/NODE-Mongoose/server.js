68915d010b675e069e3849fbb05ae10e887fd0bb
JKKook <whdrnrdldia@naver.com>
2023-04-26 23:38:06 +0900

    login from server with JWT

--- JS-NODE/NODE-Mongoose/server.js
+++ JS-NODE/NODE-Mongoose/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 6001;
 const mongoose = require('mongoose');
-const userSchemas = require('./Models/UserSchema');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config(); // env파일에서 환경변수 불러오기
 
+const { userSchemas } = require('./models/User');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // DB connect
 mongoose
@@ -45,6 +48,45 @@ app.post('/register', async (req, res) => {
     } catch (error) {
         res.status(500).send(error);
         console.log(error);
+    }
+});
+
+// // sign-In
+app.post('/login', async (req, res) => {
+    console.log('req.body', req.body);
+    // 1. 요청 된 이메일을 데이터베이스에서 찾아야 함.
+    const userEmail = await User.findOne({ email: req.body.email });
+
+    try {
+        const isEmail = await userEmail.save();
+
+        if (!isEmail) {
+            const error = new Error('이메일이 존재하지 않는데요?');
+            return res.json({ loginSuccess: false, error });
+        } else {
+            // 2. 이메일을 데이터베이스에서 찾았다면 비밀번호 비교
+            // User.js에서 comparePassword 메소드를 만들어서 사용하면 됨.
+            userEmail.comparePassword(req.body.password, (err, isMatch) => {
+                if (!isMatch) {
+                    return res.json({
+                        loginSuccess: false,
+                        message: '비밀번호가 틀렸는데요?',
+                    });
+                } else {
+                    // 3. 비밀번호까지 맞는다면 Token 형성
+                    userEmail.generateToken((err, user) => {
+                        //JSONWEBTOKEN 라이브러리
+                        if (err) return res.status(400).send(err);
+                        // 토큰 저장 : 쿠키 || 로컬스토리지
+                        res.cookie('x_auth', user.token)
+                            .status(200)
+                            .json({ loginSuccess: true, userId: user._id });
+                    });
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
     }
 });
 
