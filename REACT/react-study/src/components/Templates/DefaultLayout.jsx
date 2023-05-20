073b0aab2b3b04188e2bc9253123ae032b3d03de
JKKook <whdrnrdldia@naver.com>
2023-05-20 16:15:03 +0900

    CHORE: initial Set

---
+++ REACT/react-study/src/components/Templates/DefaultLayout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function DefaultLayout() {
+    return (
+        <>
+            <BackDrop>
+                <Layout>
+                    <Outlet></Outlet>
+                </Layout>
+            </BackDrop>
+        </>
+    );
+}
+
+const Layout = styled.main`
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    height: 100%;
+    min-height: calc(100vh - 72px);
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    background-color: #fff;
+`;
+const BackDrop = styled.div`
+    width: 100%;
+    margin-top: 72px;
+    min-height: calc(100vh - 72px);
+    background-color: var(--line-gray);
+`;
